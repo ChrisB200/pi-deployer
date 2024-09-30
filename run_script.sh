48aa180f1afcd3e9c2917805975e6f78ef8f34a7
Christopher Bonner <cbonner.dev@outlook.com>
2024-09-30 01:10:26 +0100

    recreate images

--- run_script.sh
+++ run_script.sh
@@ -4,6 +4,7 @@ conda init
 conda activate pi-deployer
 python ~/code/pi-deployer/main.py --name "$1"
 cd ~/code/hosted/"$1"
+docker compose build --no-cache
 docker compose up -d --force-recreate
 conda init
 conda deactivate
