e80097eb3875b8a4a209ad0e9d56758796558d08
Christopher Bonner <cbonner.dev@outlook.com>
2024-11-03 21:24:17 +0000

    fixed deployer

--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-conda init
-conda activate pi-deployer
+eval "$(conda shell.bash hook)"
 python ~/code/pi-deployer/main.py --name "$1"
 cd ~/code/hosted/"$1"
 docker compose build --no-cache
 docker compose up -d --force-recreate
-conda init
-conda deactivate
